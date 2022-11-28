8fb1639a7d4454e69f13e5e6f5f5ff3dfbf97ff9
HAOWEICHEN <229705080@qq.com>
2022-11-28 15:00:37 +0800

    添加测试

---
+++ app/src/test/java/com/jnu/planegameapp/gamedata/Buff_ATKTest.java
@@ -0,0 +1,19 @@
+package com.jnu.planegameapp.gamedata;
+
+import static org.junit.Assert.*;
+
+
+
+import org.junit.Test;
+
+public class Buff_ATKTest {
+
+    @Test
+    public void getBuff() {
+        InstrumentationRegistry.getTargetContext();
+    }
+
+    @Test
+    public void move() {
+    }
+}
