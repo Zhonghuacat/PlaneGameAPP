68de18cc125ee4d8e1bfb497cf41162dde3779eb
HAOWEICHEN <229705080@qq.com>
2022-12-04 16:36:04 +0800

    添加了 左右 发射子弹的等价类划分测试和重置状态功能的边界值测试

--- app/src/androidTest/java/com/jnu/planegameapp/BossTest.java
+++ app/src/androidTest/java/com/jnu/planegameapp/BossTest.java
@@ -138,10 +138,151 @@ public class BossTest {
         plane_boss.createBulletLeft(context,12,bullets);
         assertNotEquals(-1,plane_boss.state);//状态改变
 
+        //测试重置的功能 状态不改变 边界值1
+        bullets.clear();
+        plane_boss.state=-1;
+        plane_boss.setBulletNumber(20);
+        plane_boss.setAttackNumber(2);
+        plane_boss.createBulletLeft(context,12,bullets);
+        assertEquals(-1,plane_boss.state);//状态改变
+
+        //测试重置的功能 状态不改变 边界值2
+        bullets.clear();
+        plane_boss.state=-1;
+        plane_boss.setBulletNumber(2);
+        plane_boss.setAttackNumber(25);
+        plane_boss.createBulletLeft(context,12,bullets);
+        assertEquals(-1,plane_boss.state);//状态改变
+
     }
 
     @Test
     public void createBulletRight() {
+        ArrayList<Bullet> bullets = new ArrayList<>();
+        Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        Plane_boss plane_boss = new Plane_boss(context);
+        //限制的有时间、子弹数、攻击数 攻击数为0时会重置state和攻击数
+        //符合的等价类1
+        bullets.clear();
+        plane_boss.state=-1;
+        plane_boss.setBulletNumber(20);
+        plane_boss.setAttackNumber(25);
+        plane_boss.createBulletRight(context,12,bullets);
+        assertEquals(1,bullets.size());//--符合的等价类1
+
+        //符合的等价类2 边界值1
+        bullets.clear();
+        plane_boss.state=-1;
+        plane_boss.setBulletNumber(1);
+        plane_boss.setAttackNumber(25);
+        plane_boss.createBulletRight(context,12,bullets);
+        assertEquals(1,bullets.size());//--符合的等价类1
+
+        //符合的等价类3 边界值2
+        bullets.clear();
+        plane_boss.state=-1;
+        plane_boss.setBulletNumber(20);
+        plane_boss.setAttackNumber(1);
+        plane_boss.createBulletRight(context,12,bullets);
+        assertEquals(1,bullets.size());//--符合的等价类1
+
+        //时间上不符合的等价类2
+        bullets.clear();
+        plane_boss.state=-1;
+        plane_boss.setBulletNumber(20);
+        plane_boss.setAttackNumber(25);
+        plane_boss.createBulletRight(context,8,bullets);
+        assertEquals(0,bullets.size());//--时间上不符合的等价类
+
+        //时间为负的等价类3，危险
+        bullets.clear();
+        plane_boss.state=-1;
+        plane_boss.setBulletNumber(20);
+        plane_boss.setAttackNumber(25);
+        plane_boss.createBulletRight(context,-12,bullets);
+        assertEquals(1,bullets.size());
+
+        //子弹数不符合的等价类4 边界值 子弹数0
+        bullets.clear();
+        plane_boss.state=-1;
+        plane_boss.setBulletNumber(0);
+        plane_boss.setAttackNumber(25);
+        plane_boss.createBulletRight(context,12,bullets);
+        assertEquals(0,bullets.size());
+
+        //子弹数不符合的等价类4 边界值 子弹数-1
+        bullets.clear();
+        plane_boss.state=-1;
+        plane_boss.setBulletNumber(-1);
+        plane_boss.setAttackNumber(25);
+        plane_boss.createBulletRight(context,12,bullets);
+        assertEquals(0,bullets.size());
+
+        //攻击数不符合的等价类5 边界值 攻击数0
+        bullets.clear();
+        plane_boss.state=-1;
+        plane_boss.setBulletNumber(20);
+        plane_boss.setAttackNumber(0);
+        plane_boss.createBulletRight(context,12,bullets);
+        assertEquals(0,bullets.size());
+
+        //攻击数不符合的等价类5 边界值 攻击数-1
+        bullets.clear();
+        plane_boss.state=-1;
+        plane_boss.setBulletNumber(20);
+        plane_boss.setAttackNumber(-1);
+        plane_boss.createBulletRight(context,12,bullets);
+        assertEquals(0,bullets.size());
+
+        //测试状态变化----
+        //测试重置的功能 边界值1
+        bullets.clear();
+        plane_boss.state=-1;
+        plane_boss.setBulletNumber(0);
+        plane_boss.setAttackNumber(25);
+        plane_boss.createBulletRight(context,12,bullets);
+        assertNotEquals(-1,plane_boss.state);//状态改变
+
+        //测试重置的功能 边界值2
+        bullets.clear();
+        plane_boss.state=-1;
+        plane_boss.setBulletNumber(1);
+        plane_boss.setAttackNumber(25);
+        plane_boss.createBulletRight(context,12,bullets);
+        assertNotEquals(-1,plane_boss.state);//状态改变
+
+        //测试重置的功能 边界值3
+        bullets.clear();
+        plane_boss.state=-1;
+        plane_boss.setBulletNumber(20);
+        plane_boss.setAttackNumber(0);
+        plane_boss.createBulletRight(context,12,bullets);
+        assertNotEquals(-1,plane_boss.state);//状态改变
+
+        //测试重置的功能 边界值4
+        bullets.clear();
+        plane_boss.state=-1;
+        plane_boss.setBulletNumber(20);
+        plane_boss.setAttackNumber(1);
+        plane_boss.createBulletRight(context,12,bullets);
+        assertNotEquals(-1,plane_boss.state);//状态改变
+
+        //测试重置的功能 状态不改变 边界值1
+        bullets.clear();
+        plane_boss.state=-1;
+        plane_boss.setBulletNumber(20);
+        plane_boss.setAttackNumber(2);
+        plane_boss.createBulletRight(context,12,bullets);
+        assertEquals(-1,plane_boss.state);//状态改变
+
+        //测试重置的功能 状态不改变 边界值2
+        bullets.clear();
+        plane_boss.state=-1;
+        plane_boss.setBulletNumber(2);
+        plane_boss.setAttackNumber(25);
+        plane_boss.createBulletRight(context,12,bullets);
+        assertEquals(-1,plane_boss.state);//状态改变
+
     }
 
     @Test
